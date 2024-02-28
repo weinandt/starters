3122c9264943dfd7a6b53d7ec2313a99f137739a
weinandt <nick.weinandt@gmail.com>
2024-02-28 14:46:03 -0600

    able to read schema from disk

--- nodejs-typescript/src/server.ts
+++ nodejs-typescript/src/server.ts
@@ -1,5 +1,9 @@
-import path from 'node:path'
-
-// Reading the graphql schema from disk
-const pathToSchema = path.join(__dirname, 'schema.graphql')
-console.log(pathToSchema)
+import { join } from 'node:path'
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
+import { loadSchemaSync } from '@graphql-tools/load'
+import { addResolversToSchema } from '@graphql-tools/schema'
+ 
+const schema = loadSchemaSync(join(__dirname, 'schema.graphql'), {
+  loaders: [new GraphQLFileLoader()]
+})
+ 
