7f76ebb8455126dfcc36fe02adeb15d8487584bc
weinandt <nick.weinandt@gmail.com>
2024-06-20 16:20:20 -0500

    request response validation working

--- open-api/node-typescript/README.md
+++ open-api/node-typescript/README.md
@@ -1,8 +1,11 @@
 # Open API Node/Typescript
 
+## Known Issues
+- Validation: express-openapi-validator
+    - Doesn't support open api spec versions greater than 3.0.0: https://github.com/cdimascio/express-openapi-validator/issues/573
+    - Validates the spec asynchronously and doesn't return the error until the middleware is called.
+
 ## TODO:
-- Request validation
-- Response validation
 - SDK generation
     - Typescript
     - Golang
@@ -11,3 +14,6 @@
 - How to use for security testing
 - How to set up auth for docs, so users can test api in the browser
 - Determine if you need express-swagger-ui or if you can just distribute the open api spec and some html.
+
+### Tests to Write
+- Validation of request and response.
--- open-api/node-typescript/src/server.ts
+++ open-api/node-typescript/src/server.ts
@@ -2,11 +2,43 @@ import express from 'express'
 import swaggerUI from 'swagger-ui-express'
 import fs from "node:fs"
 import yaml from 'yaml'
+import * as OpenApiValidator from 'express-openapi-validator';
 
 const file  = fs.readFileSync('./openapi.yaml', 'utf8')
 const swaggerDocument = yaml.parse(file)
 const app = express()
+
+// Middleware.
+app.use((err: any, req: any, res: any, next: any) => {
+    // format error
+    res.status(err.status || 500).json({
+      message: err.message,
+      errors: err.errors,
+    });
+  });
+  
+
+
+// Routes.
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+// This will also validate the spec.
+
+const validationMiddleware = OpenApiValidator.middleware({
+  apiSpec: "./openapi.yaml",
+  validateRequests: true, // (default)
+  validateResponses: true, // false by default.
+
+})
+app.use(validationMiddleware);
+app.get('/board', (req, res) => {
+    res.json({
+        'test': 'asdf'
+    })
+})
+
+
+
+
 
 app.listen(3000, () => {
     console.log('Started. Docs at: http://localhost:3000/docs')
