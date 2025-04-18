a6de5d23daed2800faddbf1f98abaac12ff28904
weinandt <nick.weinandt@gmail.com>
2025-04-18 09:31:48 -0500

    react with typescript now has different folder structure

---
+++ front-end/react-typescript/src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { MyButton } from './components/testButton.js';
+
+export default function App() {
+    return (
+        <div>
+            <h1>Welcome to my app</h1>
+            <MyButton />
+        </div>
+    );
+}
--- front-end/react-typescript/src/index.tsx
+++ front-end/react-typescript/src/index.tsx
@@ -1,6 +1,10 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app.js';
+
 const root = createRoot(document.getElementById('root'));
-root.render(React.createElement(StrictMode, null,
-    React.createElement(App, null)));
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
